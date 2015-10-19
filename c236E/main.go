b6b0779dc83a412c71a5d64f678eee305ed1c6fb
Kunde21 <Kunde21@gmail.com>
2015-10-18 21:24:26 -0700

    Tests added.

--- c236E/main.go
+++ c236E/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+var bag = []byte{'O', 'I', 'S', 'Z', 'L', 'J', 'T'}
 
 func main() {
-	fmt.Println()
+	pick := make([]bool, len(bag))
+	otp := ""
+	r := 0
+	for i := 0; i < 50; i++ {
+		r = rand.Intn(len(bag))
+		if !(pick[r]) {
+			otp += string(bag[r])
+			pick[r] = true
+		}
+		if len(pick)%len(bag) == 0 {
+			for i := 0; i < len(pick); i++ {
+				pick[i] = false
+			}
+		}
+	}
+	fmt.Println(otp)
 }
